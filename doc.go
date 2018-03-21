7278af8ad7c025e4e8e54c0e4905d401c481176e
Doug Chimento <dchimento@gmail.com>
2018-03-21 22:25:45 +0200

    moving all projects to one dir

---
+++ doc.go
@@ -0,0 +1,131 @@
+// Copyright 2016 Douglas Chimento.  All rights reserved.
+
+/*
+Package gclient provides access to toggl REST API.
+
+Example:
+       import "gopkg.in/dougEfresh/gtoggl.v8"
+       import "ggopkg.in/dougEfresh/toggl-client.v8"
+
+       func main() {
+	    thc, err := gtoggl.NewClient("token")
+	    ...
+	    tc, err := gclient.NewClient(thc)
+	    ...
+	    client,err := tc.Get(1)
+	    if err == nil {
+	 	panic(err)
+	   }
+       }
+*/
+package tapi
+// Copyright 2016 Douglas Chimento.  All rights reserved.
+
+/*
+Package gtoggl access to toggl REST API
+
+Example:
+        import "gopkg.in/dougEfresh/gtoggl.v8"
+        import "ggopkg.in/dougEfresh/toggl-timeentry.v8"
+
+        func main() {
+	    thc, err := gtoggl.NewClient("token")
+	    ...
+	    tc, err := gtimeentry.NewClient(thc)
+	    ...
+	    timeentry,err := tc.Get(1)
+	    if err == nil {
+		panic(err)
+	    }
+	}
+*/
+// Copyright 2016 Douglas Chimento.  All rights reserved.
+
+/*
+Package gproject provides access to toggl REST API
+
+
+Example:
+       import "gopkg.in/dougEfresh/gtoggl.v8"
+       import "ggopkg.in/dougEfresh/toggl-project.v8"
+
+       func main() {
+	    thc, err := gtoggl.NewClient("token")
+	    ...
+	    tc, err := gproject.NewClient(thc)
+	    ...
+	    project,err := tc.Get(1)
+	    if err == nil {
+	 	panic(err)
+	   }
+       }
+*/
+/// Copyright 2016 Douglas Chimento.  All rights reserved.
+
+/*
+Package gtest provides test utils for gtoggl
+*/
+// Copyright 2016 Douglas Chimento.  All rights reserved.
+
+/*
+Package gtimeentry provides access to toggl REST API
+
+
+Example:
+       import "gopkg.in/dougEfresh/gtoggl.v8"
+       import "ggopkg.in/dougEfresh/toggl-timeentry.v8"
+
+       func main() {
+	    thc, err := gtoggl.NewClient("token")
+	    ...
+	    tc, err := gtimeentry.NewClient(thc)
+	    ...
+	    timeentry,err := tc.Get(1)
+	    if err == nil {
+	 	panic(err)
+	   }
+       }
+*/
+// Copyright 2016 Douglas Chimento.  All rights reserved.
+
+/*
+Package guser provides access to toggl REST API
+
+
+Example:
+       import "gopkg.in/dougEfresh/gtoggl.v8"
+       import "ggopkg.in/dougEfresh/toggl-user.v8"
+
+       func main() {
+	    thc, err := gtoggl.NewClient("token")
+	    ...
+	    tc, err := gtimeentry.NewClient(thc)
+	    ...
+	    me,err := tc.Get(false)
+	    if err == nil {
+	 	panic(err)
+	   }
+       }
+*/
+// Copyright 2016 Douglas Chimento.  All rights reserved.
+
+/*
+Package gworkspace provides access to toggl REST API
+
+
+Example:
+       import "gopkg.in/dougEfresh/gtoggl.v8"
+       import "ggopkg.in/dougEfresh/toggl-workspace.v8"
+
+       func main() {
+	    thc, err := gtoggl.NewClient("token")
+	    ...
+	    tc, err := gworkspace.NewClient(thc)
+	    ...
+	    workspace,err := tc.Get(1)
+	    if err == nil {
+	 	panic(err)
+	   }
+       }
+*/
+
